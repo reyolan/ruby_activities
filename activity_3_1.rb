b1c3c5b7e85f59c4ac08e84441287fb41bc830e0
reyolan <reynardolan@gmail.com>
2022-05-12 20:37:57 +0800

    finish activity 3

--- activity_3_1.rb
+++ activity_3_1.rb
@@ -1,3 +1,4 @@
+#Solution 1
 class Rectangle
   attr_reader :length, :width
 
@@ -29,3 +30,29 @@ puts rectangle.perimeter
 rectangle.resize(15, 30)
 puts rectangle.length
 puts rectangle.width
+
+#Solution 2
+class Profile
+  attr_accessor :full_name, :age, :work
+  attr_reader :address
+
+  def initialize(full_name, age, address, work)
+    @full_name = full_name
+    @age = age
+    @address = address
+    @work = work
+  end
+end
+
+my_profile = Profile.new('Juan', 18, 'Bulacan', 'Instructor')
+
+puts my_profile.full_name
+my_profile.full_name = 'Juan Cruz'
+
+my_profile.age = 25
+my_profile.work = 'Software Engineer'
+
+puts my_profile.full_name
+puts my_profile.age
+puts my_profile.work
+puts my_profile.address
