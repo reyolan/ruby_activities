d424ecd354cd10cab4571f894cb6280b84b6cea6
reyolan <reynardolan@gmail.com>
2022-05-19 19:44:16 +0800

    finish db activity 2

---
+++ activity_2.sql
@@ -0,0 +1,26 @@
+-- Display count of all students
+SELECT
+    COUNT(id) AS number_of_students
+FROM
+    students
+
+-- Select all students with location is Manila
+
+SELECT
+    *
+FROM
+    students
+WHERE
+    location = 'Manila'
+
+
+-- Display average age of all students
+SELECT
+    AVG(age) as average_age
+FROM
+    students
+
+-- Display all students by age in descending order
+SELECT
+*
+FROM students ORDER BY age DESC
